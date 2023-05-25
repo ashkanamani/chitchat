527d423066e539996e9e080e24a6c7fd9a42e47d
Ashkan Amani <amaniashkan2000@gmail.com>
2023-05-25 15:13:20 +0330

    add frontend

--- data/thread.go
+++ data/thread.go
@@ -21,6 +21,46 @@ type Post struct {
 }
 
 
+// format the CreatedAt date to display nicely on the screen
+func (thread *Thread) CreatedAtDate() string {
+	return thread.CreatedAt.Format("Jan 2, 2006 at 3:04pm")
+}
+
+func (post *Post) CreatedAtDate() string {
+	return post.CreatedAt.Format("Jan 2, 2006 at 3:04pm")
+}
+
+// get the number of posts in a thread
+func (thread *Thread) NumReplies() (count int) {
+	rows, err := Db.Query("SELECT count(*) FROM posts where thread_id = $1", thread.Id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		if err = rows.Scan(&count); err != nil {
+			return
+		}
+	}
+	rows.Close()
+	return
+}
+
+// get posts to a thread
+func (thread *Thread) Posts() (posts []Post, err error) {
+	rows, err := Db.Query("SELECT id, uuid, body, user_id, thread_id, created_at FROM posts where thread_id = $1", thread.Id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		if err = rows.Scan(&post.Id, &post.Uuid, &post.Body, &post.UserId, &post.ThreadId, &post.CreatedAt); err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
 
 // Create a new thread
 func (user *User) CreateThread(topic string) (conv Thread, err error) {
@@ -71,5 +111,13 @@ func ThreadByUUID(uuid string) (conv Thread, err error) {
 	conv = Thread{}
 	err = Db.QueryRow("SELECT id, uuid, topic, user_id, created_at FROM threads WHERE uuid = $1", uuid).
 		Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt)
+	return
+}
+
+// Get the user who wrote the post
+func (post *Post) User() (user User) {
+	user = User{}
+	Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1", post.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
 	return
 }
